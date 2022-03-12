9c792f2fedb24702290c4ba47c23d214cf3d4c03
SidDas8 <siddharthdas168@gmail.com>
2022-03-11 17:03:31 -0800

    Added event handler to correctly respond when pressing the enter key.

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -28,8 +28,12 @@ data.forEach((ufo_sighting) => {
 // Select the button
 var button = d3.select("#filter-btn");
 
-// Create event handler
+// Select the form
+var form = d3.select("#form");
+
+// Create event handlers for clicking the button or pressing the enter key
 button.on("click", runEnter);
+form.on("submit",runEnter);
 
 // Complete the event handler function
 function runEnter() {
